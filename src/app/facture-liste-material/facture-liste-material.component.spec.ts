fcbbee61ec54ec72d9f0f44fa5e4bc55d5aff981
“NeValeriia” <valeriia@nebylytsia.fr>
2025-06-26 16:25:12 +0200

    111

--- src/app/facture-liste-material/facture-liste-material.component.spec.ts
+++ src/app/facture-liste-material/facture-liste-material.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FactureListeMaterialComponent } from './facture-liste-material.component';
+import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {ReactiveFormsModule} from '@angular/forms';
 
 describe('FactureListeMaterialComponent', () => {
   let component: FactureListeMaterialComponent;
@@ -8,7 +15,7 @@ describe('FactureListeMaterialComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FactureListeMaterialComponent]
+      imports: [FactureListeMaterialComponent,CommonModule,MatTableModule,MatFormFieldModule,MatInputModule,MatButtonModule,MatCardModule,ReactiveFormsModule]
     })
     .compileComponents();
 
--- src/app/facture-liste/facture-liste.component.spec.ts
+++ src/app/facture-liste/facture-liste.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FactureListeComponent } from './facture-liste.component';
+import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+} from '@angular/forms';
+
 
 describe('FactureListeComponent', () => {
   let component: FactureListeComponent;
@@ -8,7 +13,9 @@ describe('FactureListeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FactureListeComponent]
+      imports: [FactureListeComponent,
+        ReactiveFormsModule]
+      
     })
     .compileComponents();
 
